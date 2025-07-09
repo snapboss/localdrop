cc520c519a42b1d75c399de1be61905b0e49ca17
SnapBoss <mbajrami95@gmail.com>
2025-07-09 16:46:39 +0200

    Use peerjs wss just for testing

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 // A public signaling server for demonstration purposes. 
 // For a production app, you should host your own.
-export const SIGNALING_SERVER_URL = 'wss://signaling.y-presence.dev/';
+export const SIGNALING_SERVER_URL = 'wss://0.peerjs.com/';
 
 // Public STUN servers provided by Google.
 export const STUN_SERVERS = {
